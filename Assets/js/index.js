c4b473d1fe7674dbfe1c3c76f7a1ab82826b2024
lukeinco <136262607+lukeinco@users.noreply.github.com>
2023-12-21 20:26:00 -0700

    improved file tree

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,16 +1,8 @@
-// GIVEN a weather dashboard with form inputs
-// WHEN I search for a city
-// THEN I am presented with current and future conditions for that city and that city is added to the search history
-// WHEN I view current weather conditions for that city
-// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
-// WHEN I click on a city in the search history
-// THEN I am again presented with current and future conditions for that city
 const btn = document.getElementById('go');
 const search = document.getElementById('search');
 const days = document.getElementById('days');
 btn.addEventListener("click", makeSearch);
+// creates every part of the day card and appends to html
 function makeDay(element) {
     let day = document.createElement('div');
     days.appendChild(day);
@@ -30,17 +22,17 @@ function makeDay(element) {
     humid.textContent = ('humidity: ' + element.main.humidity);
     day.appendChild(humid);
 }
-
+// takes in user input and inserts into api url. Then fetches with api url
 function makeSearch() {
     const cityName = search.value;
-    const url =  (`http://api.openweathermap.org/data/2.5/forecast?q=` + cityName + `&appid=4be41e2f4ed7079b1bfec03b91bc6b39`);
+    const url =  (`https://api.openweathermap.org/data/2.5/forecast?q=` + cityName + `&appid=4be41e2f4ed7079b1bfec03b91bc6b39`);
     console.log(cityName);
     console.log(url);
     fetch(url).then(function (res) {
         return res.json();
     }).then(function (data) {
         console.log(data)
-        if (!data) {
+        if (data.message === "city not found") {
             let p = document.createElement("p");
             today.appendChild(p);
             p.textContent = "no data"
---
+++ README.md
@@ -0,0 +1,29 @@
+
+  # Weather API
+
+  ## Description
+  This is a 5 day weather forecast for any city.
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  - [License](#license)
+  - [Contact](#contact)
+
+  ## Installation
+  Simply click on the link! [link](https://lukeinco.github.io/weather-api-app)
+
+  ## Usage
+  Type the name of city you would like weather data for and read data
+
+  ## License
+  This Project is licenced under MIT License.
+
+  ## Questions
+  Questions? Contact:
+  Email: lukeinco@gmail.com
+  GitHub: https://github.com/lukeinco
+  
